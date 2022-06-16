1265fd2858032e0a722a18552e865a998d863475
Bonani Brian Wogqoyi <bonani.wogqoyi@outlook.com>
2022-06-16 19:46:24 +0200

    feature: added a spinner modal to be shown when uploading images

--- nuxt-web/pages/admin/properties/index.vue
+++ nuxt-web/pages/admin/properties/index.vue
@@ -6,7 +6,8 @@ const router = useRouter();
 // vars for the image upload
 let singleFile = null;
 let multipleFiles = null;
-let propertyDetails = ref({});
+const propertyDetails = ref({});
+const isActive = ref(false);
 
 function onSingleFileChange (event) {
   singleFile = event.target.files[0];
@@ -29,12 +30,13 @@ function onMultipleFilesChange (event) {
 }
 
 const uploadMultipleFiles = function() {
-   try {
+  try {
     const data = new FormData();
   
     for(let i=0; (multipleFiles!=null && i<multipleFiles.length); i+=1) {
       data.append('files', multipleFiles[i])
     }
+    isActive.value = true;
 
     axios.post('https://api.bestplace.co.za/upload', data, {
       headers: { 'Access-Control-Allow-Origin': '*' }
@@ -46,6 +48,7 @@ const uploadMultipleFiles = function() {
       }
     })
   } catch(e) {
+    isActive.value = false;
     throw e 
   }
 }
@@ -73,6 +76,32 @@ const persistPropertyInfo = function(files, filesDir) {
 </script>
 
 <template>
+<div class="flex flex-col space-y-4 min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-gray-900 opacity-90"
+  :class="[isActive ? 'block' : 'hidden']">
+  <div class="flex flex-col p-8 bg-white shadow-md hover:shodow-lg rounded-2xl ">
+  	<div class="flex items-center justify-between">
+  		<div class="flex items-center">
+  			<svg xmlns="http://www.w3.org/2000/svg"
+  				class="w-16 h-16 rounded-2xl p-3 border border-blue-100 text-blue-400 bg-blue-50" fill="none"
+  				viewBox="0 0 24 24" stroke="currentColor">
+  				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+  					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+  			</svg>
+  			<div class="flex flex-col ml-3">
+  				<div class="font-medium leading-none text-xl">Add new property</div>
+  				<p class="text-sm text-gray-600 leading-none mt-1 text-center">
+            Please be patient while we complete this task
+  				</p>
+  			</div>
+  		</div>
+  	</div>
+    <div class="flex justify-center items-center h-full">
+      <img class="h-16 w-16" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="">
+    </div>
+  </div>
+</div>
+
+
 <div class="w-full bg-gray-100 py-2">
   <div class="w-full lg:w-4/5 mx-auto pb-10 bg-gray-100">
     <!-- Propert Listings -->
@@ -212,7 +241,7 @@ const persistPropertyInfo = function(files, filesDir) {
       
       <div class="py-3 bg-gray-50 text-right space-x-4">
         <!-- @click="$router.push('/admin/')" -->
-        <button @click="" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-[#007bfc] hover:bg-gradient-to-r hover:from-[#007bfc] hover:to-[#51b9ff] focus:outline-none ">
+        <button @click="isActive = !isActive" type="button" class="hidden inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-[#007bfc] hover:bg-gradient-to-r hover:from-[#007bfc] hover:to-[#51b9ff] focus:outline-none ">
           <span class="font-medium text-lg">Update</span>
         </button>
 
